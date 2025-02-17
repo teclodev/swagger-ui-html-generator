3ace2f0709d4133db5e91ff90f9f3cd0eecb5e44
brendangoldberg <goldberg.brendan@gmail.com>
2025-02-17 08:21:48 -0700

    chore: dev setup

---
+++ prettier.config.mjs
@@ -0,0 +1,12 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  trailingComma: "none",
+  semi: false,
+  singleQuote: false,
+  printWidth: 120
+}
+
+export default config
