c46253b55e728b03cec607a03b849aaa55b6f9d5
brendangoldberg <goldberg.brendan@gmail.com>
2025-02-18 09:19:53 -0700

    chore: cleaner init

---
+++ example/index.ts
@@ -0,0 +1,88 @@
+import swaggerUI from "swagger-ui-express"
+import { generateSwaggerUI } from "@teclo/swagger-ui-html-generator"
+import express from "express"
+
+const doc = {
+  openapi: "3.0.0",
+  info: {
+    title: "Basic API",
+    description: "Simple API documentation with basic paths",
+    version: "1.0.0",
+  },
+  paths: {
+    "/users": {
+      get: {
+        summary: "Get a list of users",
+        description: "Returns a list of users",
+        responses: {
+          "200": {
+            description: "A list of users",
+            content: {
+              "application/json": {
+                schema: {
+                  type: "array",
+                  items: {
+                    type: "object",
+                    properties: {
+                      id: { type: "integer" },
+                      name: { type: "string" },
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+    "/users/{id}": {
+      get: {
+        summary: "Get a user by ID",
+        description: "Returns a single user",
+        parameters: [
+          {
+            name: "id",
+            in: "path",
+            required: true,
+            schema: {
+              type: "integer",
+            },
+          },
+        ],
+        responses: {
+          "200": {
+            description: "User object",
+            content: {
+              "application/json": {
+                schema: {
+                  type: "object",
+                  properties: {
+                    id: { type: "integer" },
+                    name: { type: "string" },
+                  },
+                },
+              },
+            },
+          },
+          "404": {
+            description: "User not found",
+          },
+        },
+      },
+    },
+  },
+}
+
+const html = generateSwaggerUI({
+  swaggerDoc: doc,
+})
+
+const app = express()
+
+app.get("/docs", (req, res) => {
+  res.set("Content-Type", "text/html").send(html)
+})
+
+app.use("/swagger-ui", swaggerUI.serve, swaggerUI.setup(doc))
+
+app.listen(3000, () => console.log("listening on port 3000"))
--- index.ts
+++ index.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 // noinspection JSUnresolvedReference
 
 import packageJSON from "./package.json"
@@ -8,15 +7,12 @@ function defaultSwaggerUIVersion() {
 }
 
 function getCDN(version: string): string {
-  return `https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/${version}`
+  return `https://unpkg.com/swagger-ui-dist@${version}`
 }
 
 function generateSwaggerUI(params: { swaggerDoc: object; swaggerUiVersion?: string; customOptions?: object }): string {
   const base = getCDN(params.swaggerUiVersion ?? process.env.SWAGGER_UI_VERSION ?? defaultSwaggerUIVersion())
-  const options = stringify({
-    swaggerDoc: params.swaggerDoc,
-    customOptions: params.customOptions ?? {},
-  })
+  const swaggerDoc = params.swaggerDoc
   return `
 <!DOCTYPE html>
 <html lang="en">
@@ -27,107 +23,52 @@ function generateSwaggerUI(params: { swaggerDoc: object; swaggerUiVersion?: stri
   <style>
     html
     {
-      box-sizing: border-box
-      overflow: -moz-scrollbars-vertical
-      overflow-y: scroll
+      box-sizing: border-box;
+      overflow: -moz-scrollbars-vertical;
+      overflow-y: scroll;
     }
     *,
     *:before,
     *:after
     {
-      box-sizing: inherit
+      box-sizing: inherit;
     }
 
     body {
-      margin:0
-      background: #fafafa
+      margin: 0;
+      background: #fafafa;
     }
+    
+    .swagger-ui .topbar .download-url-wrapper { display: none }
   </style>
 </head>
 
 <body>
 <div id="swagger-ui"></div>
-<script src="${base}/swagger-ui-bundle.js"></script>
-<script src="${base}/swagger-ui-standalone-preset.js"></script>
+<script src="${base}/swagger-ui-bundle.js" crossorigin></script>
+<script src="${base}/swagger-ui-standalone-preset.js" crossorigin></script>
 <script>
 window.onload = function() {
-  var url = window.location.search.match(/url=([^&]+)/);
-  if (url && url.length > 1) {
-    url = decodeURIComponent(url[1]);
-  } else {
-    url = window.location.origin;
-  }
-  ${options}
-  url = options.swaggerUrl || url
-  var urls = options.swaggerUrls
-  var customOptions = options.customOptions
-  var spec1 = options.swaggerDoc
-  var swaggerOptions = {
-    spec: spec1,
+  const url = window.location.origin
+  window.ui = SwaggerUIBundle({
+    spec: ${JSON.stringify(swaggerDoc)},
     url: url,
-    urls: urls,
-    dom_id: "#swagger-ui",
+    dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
       SwaggerUIBundle.presets.apis,
       SwaggerUIStandalonePreset
     ],
-    plugins: [],
-    layout: "BaseLayout"
-  }
-  for (var attrname in customOptions) {
-    swaggerOptions[attrname] = customOptions[attrname];
-  }
-  var ui = SwaggerUIBundle(swaggerOptions)
-
-  if (customOptions.oauth) {
-    ui.initOAuth(customOptions.oauth)
-  }
-
-  if (customOptions.preauthorizeApiKey) {
-    const key = customOptions.preauthorizeApiKey.authDefinitionKey;
-    const value = customOptions.preauthorizeApiKey.apiKeyValue;
-    if (!!key && !!value) {
-      const pid = setInterval(() => {
-        const authorized = ui.preauthorizeApiKey(key, value);
-        if(!!authorized) clearInterval(pid);
-      }, 500)
-    }
-  }
-
-  if (customOptions.authAction) {
-    ui.authActions.authorize(customOptions.authAction)
-  }
-
-  window.ui = ui
+    plugins: [
+      SwaggerUIBundle.plugins.DownloadUrl
+    ],
+    layout: "StandaloneLayout"
+  })
 }
 </script>
 </body>
 </html>
 `
-}
-
-/**
- * @see https://github.com/scottie1984/swagger-ui-express/blob/master/index.js
- */
-function stringify(obj: any) {
-  const placeholder = "____FUNCTIONPLACEHOLDER____"
-  const fns: any[] = []
-  let json = JSON.stringify(
-    obj,
-    function (_, value) {
-      if (typeof value === "function") {
-        fns.push(value)
-        return placeholder
-      }
-      return value
-    },
-    2,
-  )
-  json = json.replace(new RegExp('"' + placeholder + '"', "g"), function () {
-    return fns.shift()
-  })
-  return "var options = " + json + ";"
 }
 
 export { generateSwaggerUI }
